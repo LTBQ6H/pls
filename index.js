75de2e1e3ec8928480d3ae1c4fa1faec3810060a
SmartPLS <149936880+LTBQ6H@users.noreply.github.com>
2026-02-14 09:32:27 +0700

    1
    1

---
+++ index.js
@@ -0,0 +1,97 @@
+// console.clear();
+
+let contentTitle;
+
+function dynamicClothingSection(ob) {
+  let boxDiv = document.createElement("div");
+  boxDiv.id = "box";
+
+  let boxLink = document.createElement("a");
+  // boxLink.href = '#'
+  boxLink.href = "product/" + ob.id;
+  // console.log('link=>' + boxLink);
+
+  let imgTag = document.createElement("img");
+  // imgTag.id = 'image1'
+  // imgTag.id = ob.photos
+  imgTag.src = ob.preview;
+
+  let detailsDiv = document.createElement("div");
+  detailsDiv.id = "details";
+
+  let h3 = document.createElement("h3");
+  let h3Text = document.createTextNode(ob.Name);
+  h3.appendChild(h3Text);
+
+  let h4 = document.createElement("h4");
+  let h4Text = document.createTextNode(ob.Brand);
+  h4.appendChild(h4Text);
+
+  let h2 = document.createElement("h2");
+  let h2Text = document.createTextNode("VNĐ  " + ob.Price);
+  h2.appendChild(h2Text);
+
+  boxDiv.appendChild(boxLink);
+  boxLink.appendChild(imgTag);
+  boxLink.appendChild(detailsDiv);
+  detailsDiv.appendChild(h3);
+  detailsDiv.appendChild(h4);
+  detailsDiv.appendChild(h2);
+
+  return boxDiv;
+}
+
+//  TO SHOW THE RENDERED CODE IN CONSOLE
+// console.log(dynamicClothingSection());
+
+// console.log(boxDiv)
+
+let mainContainer = document.getElementById("mainContainer");
+let containerClothing = document.getElementById("containerClothing");
+let containerAccessories = document.getElementById("containerAccessories");
+// mainContainer.appendChild(dynamicClothingSection('hello world!!'))
+
+// BACKEND CALLING
+// httpRequest.open(
+//   "GET",
+//   "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
+//   true
+// );
+// httpRequest.send();
+function searchProduct(){
+  query = document.getElementById("input").value
+  callSearchAPI(query)
+}
+
+function callSearchAPI(query){
+  let query_str = ""
+  if (query && query.length > 0) {
+    query_str = "?query=" + query
+  }
+  fetch("https://script.google.com/macros/s/AKfycbww_fcmASpd2V7aBkxJrmhN65Vz6w45_A6b1LSDEmViuCz2OXcb7OSFmEGMyvtuQWwfUA/exec" + query_str)
+  .then(res => res.json())
+  .then(res => {
+    contentTitle = res['data']
+  
+    let cart_infor = JSON.parse(window.localStorage.getItem("cart_infor") ? window.localStorage.getItem("cart_infor") : JSON.stringify({"list": []}))
+    document.getElementById("badge").innerHTML = cart_infor.list.length
+
+    containerAccessories.innerHTML = ""
+    containerClothing.innerHTML = ""
+    for (let i = 0; i < contentTitle.length; i++) {
+      if (contentTitle[i].Is_Accessory === "t") {
+        console.log(contentTitle[i]);
+        containerAccessories.appendChild(
+          dynamicClothingSection(contentTitle[i])
+        );
+      } else {
+        console.log(contentTitle[i]);
+        containerClothing.appendChild(
+          dynamicClothingSection(contentTitle[i])
+        );
+      }
+    }
+  })
+}
+
+callSearchAPI("")
---
+++ routes.js
@@ -0,0 +1,133 @@
+const appVue = new Vue({
+    el: '#root',
+    data: {
+        url1: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQklakpFO9H56h1UHHgsQNwhQrwArgNm5MyNPbf-6QUtLKU3UWFm_cnNDqFkBltEj2CqkCnjIyUdRAv/pubhtml?gid=1352438841&single=true",
+        url2: "https://script.google.com/macros/s/AKfycbww_fcmASpd2V7aBkxJrmhN65Vz6w45_A6b1LSDEmViuCz2OXcb7OSFmEGMyvtuQWwfUA/exec",
+        paths: "",
+        search_query: "",
+        productList: [],
+        cart_infor: {"list": []},
+        totalAmount: 0,
+        id_preview: 0,
+        inCart: false,
+        user: {
+            name: "",
+            phone: "",
+            email: "",
+            address: "",
+        },
+        order: {
+            status: "",
+            code: "",
+            list: [],
+            totalAmount: 0,
+            user: {
+                name: "",
+                phone: "",
+                email: "",
+                address: "",
+            }
+        }
+    },
+    created () {
+
+        fetch(this.url1)
+        .then(res => res.json())
+        .then(res => {
+            this.productList = res['data']
+
+            let cart_infor = JSON.parse(window.localStorage.getItem("cart_infor") ? window.localStorage.getItem("cart_infor") : JSON.stringify({"list": []}))
+            this.cart_infor = cart_infor
+            document.getElementById("badge").innerHTML = cart_infor.list.length
+
+            let totalAmount = 0
+            for (let i = 0; i < cart_infor.list.length; i++) {
+                totalAmount += Number(this.productList[cart_infor.list[i].id-1].price) * cart_infor.list[i].counter
+            }
+            this.totalAmount = totalAmount
+            let paths = window.location.pathname.split('/')
+            this.paths = paths.slice(1)
+            if (this.paths[0] === "order") {
+                fetch(`${this.url2}?code=` + this.paths[1], {"method": "POST"})
+                .then(res => res.json())
+                .then(res => {
+                    this.order = res
+                    totalAmount = 0
+                    for (let i = 0; i < this.order.list.length; i++) {
+                        totalAmount += Number(this.productList[this.order.list[i].id-1].price) * this.order.list[i].counter
+                    }
+                    this.order.totalAmount = totalAmount
+                })
+            } else if (this.paths[0] === "product") {
+                for (let i = 0; i < cart_infor.list.length; i++) {
+                    if (this.paths[1] == cart_infor.list[i].id) {
+                        this.inCart = true
+                        break
+                    }
+                }
+            }
+
+            document.getElementById("badge").innerHTML = cart_infor['list'].length
+        })
+    },
+    methods : {
+        changePreview(id) {
+            this.id_preview = id
+        },
+        searchProduct() {
+            fetch(this.url1 + "?query=" + this.search_query)
+            .then(res => res.json())
+            .then(res => {
+                this.productList = res['data']
+            })
+        },
+        addToCart(id) {
+            let cart = JSON.stringify({"list":[{"id":id, "counter":1}]})
+            let cart_infor = JSON.parse(window.localStorage.getItem("cart_infor") ? window.localStorage.getItem("cart_infor") : cart)
+            if (window.localStorage.getItem("cart_infor")) {
+                let has_item = false
+                for (let i = 0; i < cart_infor.list.length; i ++) {
+                    if (cart_infor.list[i].id == id) {
+                        cart_infor.list[i].counter += 1
+                        has_item = true
+                        break;
+                    }
+                }
+                if (!has_item) {
+                    cart_infor.list.push(JSON.parse(cart).list[0])
+                }
+            }
+            window.localStorage.setItem("cart_infor", JSON.stringify(cart_infor))
+            this.cart_infor = cart_infor
+            window.location.reload()
+        },
+        placeOrder() {
+            fetch(`${this.url2}?name=${this.user.name}&phone=${this.user.phone}&email=${this.user.email}&address=${this.user.address}&order_content=${window.localStorage.getItem("cart_infor")}`, {method: "POST"})
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                if(res['result'] === 'success') {
+                    window.localStorage.setItem("cart_infor", JSON.stringify({"list": []}))
+                    this.cart_infor = {"list": []}
+                    window.location.replace('/orderPlaced')
+                }
+            })
+        },
+        saveCart() {
+            window.localStorage.setItem("cart_infor", JSON.stringify(this.cart_infor))
+            let totalAmount = 0
+            for (let i = 0; i < this.cart_infor.list.length; i++) {
+                totalAmount += Number(this.productList[this.cart_infor.list[i].id-1].price) * this.cart_infor.list[i].counter
+            }
+            this.totalAmount = totalAmount
+        },
+        deleteCart() {
+            window.localStorage.setItem("cart_infor", JSON.stringify({"list": []}))
+            this.cart_infor = {"list": []}
+            window.location.reload()
+        },
+        redirect(url) {
+            window.location.replace(url)
+        }
+    }
+})
